5c0f56dbe93dbbd53ff67d6f01d5b011fac9a845
crazymaax <oliveira.maxmilan@gmail.com>
2022-09-07 01:14:25 -0300

    feat: Global Style added to main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { GlobalStyle } from './styles/Global';
 import App from './App'
 
 import { BrowserRouter } from "react-router-dom";
@@ -7,6 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
     <React.StrictMode>
+      <GlobalStyle />
       <App />
     </React.StrictMode>
   </BrowserRouter>
