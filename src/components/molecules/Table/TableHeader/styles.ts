84c7288d7c26700ad0887a271c226e11e880d3f3
crazymaax <oliveira.maxmilan@gmail.com>
2022-09-07 13:54:13 -0300

    style: Correcting table size and adjusting span positions.

--- src/components/molecules/Table/TableHeader/styles.ts
+++ src/components/molecules/Table/TableHeader/styles.ts
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    max-width: calc(945px - 40px);
     width: 100%;
+    padding: 16px;
+    margin-right: 24px;
 
     display: flex;
-    justify-content: space-between;
+    gap: 10px;
+
     align-items: center;
+
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+
+    span {
+        width: 166px;
+    }
 `
