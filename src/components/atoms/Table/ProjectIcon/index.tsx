1ad5012f7e8a07dd9b08dcce3ee15e5477bb3d27
crazymaax <oliveira.maxmilan@gmail.com>
2022-09-07 01:17:38 -0300

    feat: Listed project icon displayed as a atom.

---
+++ src/components/atoms/Table/ProjectIcon/index.tsx
@@ -0,0 +1,13 @@
+import { Icon } from "./style";
+
+const ProjectIcon = () => {
+
+    return (
+        <Icon>
+            {/* <img src="" alt="" /> */} {/* Imagino que aqui esteja a imagem do projeto, portanto deveria haver uma tag <img/> aqui */}
+            
+        </Icon>
+    )
+}
+
+export default ProjectIcon;
---
+++ src/components/atoms/Table/ProjectIcon/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Icon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 8px;
+    background-color: var(--red)
+
+`
