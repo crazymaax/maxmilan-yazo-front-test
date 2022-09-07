32423126a9fad84643d934865fa03a0a3f2ab06b
crazymaax <oliveira.maxmilan@gmail.com>
2022-09-07 01:16:54 -0300

    feat: CheckBox atom added.

---
+++ src/components/atoms/CheckBox/index.tsx
@@ -0,0 +1,10 @@
+import { Input } from "./styles";
+
+const CheckBox = () => {
+
+    return (
+        <Input type="checkbox" name="" id="" />
+    )
+}
+
+export default CheckBox;
---
+++ src/components/atoms/CheckBox/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+    width: 16px;
+    height: 16px;
+    opacity: 0.75;
+`
