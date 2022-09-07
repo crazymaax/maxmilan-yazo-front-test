a3266adb4e1838bb1dd076c3e221a3e4db9fc2f4
crazymaax <oliveira.maxmilan@gmail.com>
2022-09-07 01:23:14 -0300

    feat: My Projects organism added to app.

---
+++ src/components/organisms/MyProjectsOrganism/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+    width: 945px;
+    height: 100vh;
+    padding-left: 40px;
+    padding-top: 40px;
+
+    h1 {
+        margin-bottom: 40px;
+    }
+
+`
