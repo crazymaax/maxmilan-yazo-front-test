349d61e6c74a550937d01fc4c2572e9d95b0463f
crazymaax <oliveira.maxmilan@gmail.com>
2022-09-07 13:45:11 -0300

    style: Hover effects and cursor pointer on Dots icon.

--- src/components/atoms/Icons/DotsIcon/styles.ts
+++ src/components/atoms/Icons/DotsIcon/styles.ts
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
 export const Image = styled.img`
-
-    
     border: 1px solid rgba(0, 0, 0, 0.1);
     border-radius: 8px;
 
+    cursor: pointer;
+    transition: box-shadow 0.3s;
+    &:hover {
+        box-shadow: 0px 3px 6px #FD9F2B2F;
+    }
 `
