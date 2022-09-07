347ed53403e6308ce491be972018a52ea30561d7
crazymaax <oliveira.maxmilan@gmail.com>
2022-09-07 13:55:07 -0300

    style: Adjusting width and turning it to fixed position.

--- src/components/organisms/Aside/styles.ts
+++ src/components/organisms/Aside/styles.ts
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const AsideContainer = styled.aside`
-    width: 235px;
-    height: 100vh;
+    width: calc(235px - 80px - 2px); // Tamanho ideal - padding lateral - borda.
+    height: calc(100vh - 120px); // 100vh - 40 paddingtop - 80 paddingbottom.
 
     display: flex;
     flex-direction: column;
@@ -12,6 +12,10 @@ export const AsideContainer = styled.aside`
     padding: 40px;
     padding-top: 80px;
     border: 1px solid #0000001A;
+    background-color: var(--white);
+    
+    position: fixed;
+    z-index: 1;
 
     li {
         & + li {
